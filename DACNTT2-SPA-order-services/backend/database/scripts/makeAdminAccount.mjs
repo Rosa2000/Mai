51952cf093c37bb1fe794d5e4a1857190e08117b
Mei Mei <63657257+mmmaaaiii2202@users.noreply.github.com>
2022-07-24 23:28:49 +0700

    test

--- DACNTT2-SPA-order-services/backend/database/scripts/makeAdminAccount.mjs
+++
@@ -1,5 +0,0 @@
-import { createUser } from "../../controller/database/userController.mjs";
-import { encryptText } from "../../middleware/signUpHelper.mjs";
-const password = await encryptText("abcdef");
-
-await createUser("0773053503", password, "ADMIN", "ADMIN");
--- DACNTT2-SPA-order-services/backend/database/scripts/scripts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-./dbUser.sh
-echo "CREATED USER DATABASE"
-./dbProduct.sh
-echo "CREATED PRODUCT DATABASE"
-./dbOrders.sh
-echo "CREATED ORDER DATABASE"
-./dbServices.sh
-echo "CREATED SERVICES DATABASE"
